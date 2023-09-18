b92d1c7f706ce977d4fa0a538271839cf8aa645e
EC2 Default User <ec2-user@ip-172-31-30-6.ap-northeast-1.compute.internal>
2023-09-18 20:11:46 +0000

    [24-midle]

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   root to: "homes#top"
   devise_for :users
 
-resources :post_images, only: [:new, :index, :show, :create]
+resources :post_images, only: [:new, :create, :index, :show, :create, :destroy] do
+  resource :favorites, only: [:create, :destroy]
+  resources :post_comments, only: [:create, :destroy]
+end
+resources :users, only: [:show, :edit, :update]
 
   get "/homes/about" => 'homes#about', as: 'about'
 end
